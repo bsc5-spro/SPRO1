766a166a5e33b218b3da03fb9917b5557fd9f80e
Bacso Adam <adamgabor2005@gmail.com>
2025-10-13 14:17:38 +0200

    measure ticks between two obstruction, respecting overflow

---
+++ lib/Opto/opto.h
@@ -0,0 +1,10 @@
+#ifndef OPTO
+
+#define OPTO
+
+void opto_init(void);
+unsigned long get_delta_ticks(void); // return time between two obstructions in ms
+
+// unsigned int get_n_average(int n);
+
+#endif
