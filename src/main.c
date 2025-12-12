d45199101e5e2f6d705e62794daa92493144af72
Bacso Adam <adamgabor2005@gmail.com>
2025-12-12 19:18:24 +0100

    update final values and make run loop indefinitely

--- src/main.c
+++ src/main.c
@@ -29,86 +29,89 @@ int main(void) {
 
   init_display();
 
-  char runSelected = 0;
-  while (!runSelected) {
-    int action = read_value();
-    switch (action) {
-    case 1: {
-      fixed number = read_numpad();
-      targetDistance = (uint16_t)(number.f_number * 10);
-      set_value("distance", number.i_number);
-      set_property("distance", "vvs1", number.decimalPlace);
-      break;
-    }
-    case 2: {
-      fixed number = read_numpad();
-      targetTime = (uint16_t)(number.f_number * 10);
-      set_value("time", number.i_number);
-      set_property("time", "vvs1", number.decimalPlace);
-      break;
-    }
-    case 0:
-      // only continue on if both dist and time are nonzero
-      if (targetDistance != 0 && targetTime != 0) {
-        runSelected = 1;
-        set_page(2);
+  while (1) {
+    char runSelected = 0;
+    while (!runSelected) {
+      int action = read_value();
+      switch (action) {
+      case 1: {
+        fixed number = read_numpad();
+        targetDistance = (uint16_t)(number.f_number * 10);
+        set_value("distance", number.i_number);
+        set_property("distance", "vvs1", number.decimalPlace);
+        break;
       }
-      break;
-
-    default:
-      break;
-    }
-  }
-
-  // CONTROL //
-
-  toggle_recording();
-
-  char currentPWM = 80;
-  // TODO: change according to tests for average run
-  pwm1_set_duty(currentPWM); // initial acceleration
-
-  int velocityDeviation = get_average_velocity() - get_current_velocity();
-
-  char loopCount = 0;
-
-  while (get_distance_travelled() < targetDistance) {
-    loopCount++;
-    monitor_encoder();
-
-    if (loopCount % 6 == 0) {
-      // check if velocity stabilized
-      if (abs(velocityDeviation) < vError) {
-        uint16_t distanceRemaining =
-            targetDistance - get_distance_travelled();          // mm
-        uint16_t timeRemaining = targetTime - get_total_time(); // 1/100 s
-        timeRemaining /= 100; // convert from 1/100 s to s
-
-        uint16_t requiredVelocity = distanceRemaining / timeRemaining; // mm/s
-
-        int deltaV = requiredVelocity - get_average_velocity();
-
-        if (abs(deltaV) < vError) {
-          continue;
-        } else if (deltaV < 0) { // TODO: make proportional
-          currentPWM -= 10;
-        } else {
-          currentPWM += 10;
+      case 2: {
+        fixed number = read_numpad();
+        targetTime = (uint16_t)(number.f_number * 10);
+        set_value("time", number.i_number);
+        set_property("time", "vvs1", number.decimalPlace);
+        break;
+      }
+      case 0:
+        // only continue on if both dist and time are nonzero
+        if (targetDistance != 0 && targetTime != 0) {
+          runSelected = 1;
+          set_page(2);
         }
-        pwm1_set_duty(currentPWM);
+        break;
+
+      default:
+        break;
       }
     }
 
-    if (loopCount % 15 == 0) {
-      loopCount = 0;
-      update_run_screen(get_distance_travelled(), get_total_time(),
-                        get_average_velocity());
+    // CONTROL //
+
+    start_recording();
+
+    char currentPWM = 80;
+    // TODO: change according to tests for average run
+    pwm1_set_duty(currentPWM); // initial acceleration
+
+    int velocityDeviation = get_average_velocity() - get_current_velocity();
+
+    char loopCount = 0;
+
+    while (get_distance_travelled() < targetDistance) {
+      loopCount++;
+      monitor_encoder();
+
+      if (loopCount % 6 == 0) {
+        // check if velocity stabilized
+        if (abs(velocityDeviation) < vError) {
+          uint16_t distanceRemaining =
+              targetDistance - get_distance_travelled();          // mm
+          uint16_t timeRemaining = targetTime - get_total_time(); // 1/100 s
+          timeRemaining /= 100; // convert from 1/100 s to s
+
+          uint16_t requiredVelocity = distanceRemaining / timeRemaining; // mm/s
+
+          int deltaV = requiredVelocity - get_average_velocity();
+
+          if (abs(deltaV) < vError) {
+            continue;
+          } else if (deltaV < 0) { // TODO: make proportional
+            currentPWM -= 10;
+          } else {
+            currentPWM += 10;
+          }
+          pwm1_set_duty(currentPWM);
+        }
+      }
+
+      if (loopCount % 15 == 0) {
+        loopCount = 0;
+        update_run_screen(get_distance_travelled(), get_total_time(),
+                          get_average_velocity());
+      }
     }
+
+    // END RUN //
+    pwm1_set_duty(0);
+    stop_recording();
+    update_run_screen(get_distance_travelled(), get_total_time(), 0);
   }
-
-  pwm1_set_duty(0);
-
-  // END RUN //
   return 0;
 }
 
