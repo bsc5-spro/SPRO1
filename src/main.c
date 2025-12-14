2272152f1a940ba47a48fad75717a0f38c6bbcc9
Bacso Adam <adamgabor2005@gmail.com>
2025-12-14 18:12:57 +0100

    fix program flow issues

--- src/main.c
+++ src/main.c
@@ -32,6 +32,9 @@ int main(void) {
   clear_buffer();
 
   pwm1_init();
+  pwm1_set_duty(50);
+  _delay_ms(1000);
+  pwm1_set_duty(0);
 
   fixed distance, time;
 
@@ -43,14 +46,16 @@ int main(void) {
   time.i_number = 505;
   time.decimalPlace = 1;
 
-  update_main_page(distance, time);
+  targetDistance = (uint16_t)(distance.f_number * 10);
+  targetTime = (uint16_t)(time.f_number * 10);
 
   // echo_serial();
 
   while (1) {
+    update_main_page(distance, time);
+
     char runSelected = 0;
     while (!runSelected) {
-      clear_buffer();
       char action = read_value();
       // printf("%c%c%c%c", action, 255, 255, 255);
       switch (action) {
@@ -67,7 +72,7 @@ int main(void) {
         update_main_page(distance, time);
         break;
       }
-      case 0x0:
+      case 0xa:
         // only continue on if both dist and time are nonzero
         if (targetDistance != 0 && targetTime != 0) {
           runSelected = 1;
@@ -100,9 +105,17 @@ int main(void) {
 
     int velocityDeviation = get_average_velocity() - get_current_velocity();
 
-    char loopCount = 0;
+    uint8_t loopCount = 0;
 
-    while (get_distance_travelled() < targetDistance) {
+    while (1
+           //
+           // (get_distance_travelled() < targetDistance)
+           // &&
+           //
+    ) {
+      if (check_run_end()) {
+        break;
+      }
       loopCount++;
       monitor_encoder();
 
@@ -114,7 +127,8 @@ int main(void) {
           uint16_t timeRemaining = targetTime - get_total_time(); // 1/100 s
           timeRemaining /= 100; // convert from 1/100 s to s
 
-          uint16_t requiredVelocity = distanceRemaining / timeRemaining; // mm/s
+          uint16_t requiredVelocity =
+              distanceRemaining / timeRemaining; //      mm / s
 
           int deltaV = requiredVelocity - get_average_velocity();
 
@@ -134,12 +148,22 @@ int main(void) {
         update_run_screen(get_distance_travelled(), get_total_time(),
                           get_average_velocity());
       }
+      // _delay_ms(10);
     }
 
     // END RUN //
     pwm1_set_duty(0);
     stop_recording();
     update_run_screen(get_distance_travelled(), get_total_time(), 0);
+    update_run_screen(get_distance_travelled(), get_total_time(), 90);
+
+    char reset = 0;
+    while (!reset) {
+      if (read_value() == 0xc) {
+        set_page(0);
+        reset = 1;
+      }
+    }
   }
   return 0;
 }
